648b35673eb1cf175a3a8f5d7f33a4c9c51be6da
Ikem <ikem.ezechukwu@outlook.com>
2022-11-08 03:25:17 +0100

    Configured logger for prod and dev environments.

--- backend/src/utils/logger.ts
+++ backend/src/utils/logger.ts
@@ -5,12 +5,7 @@ import winstonDaily from 'winston-daily-rotate-file';
 import { LOG_DIR } from '@config';
 import { GraphQLError } from 'graphql';
 
-// logs dir
-const logDir: string = join(__dirname, LOG_DIR);
 
-if (!existsSync(logDir)) {
-  mkdirSync(logDir);
-}
 
 // Define log format
 const logFormat = winston.format.printf(({ timestamp, level, message }) => `${timestamp} ${level}: ${message}`);
@@ -21,43 +16,56 @@ const logFormat = winston.format.printf(({ timestamp, level, message }) => `${ti
  * Log Level
  * error: 0, warn: 1, info: 2, http: 3, verbose: 4, debug: 5, silly: 6
  */
-const devLogger = winston.createLogger({
-  format: winston.format.combine(
-    winston.format.timestamp({
-      format: 'YYYY-MM-DD HH:mm:ss',
-    }),
-    logFormat,
-  ),
-  transports: [
-    // debug log setting
-    new winstonDaily({
-      level: 'debug',
-      datePattern: 'YYYY-MM-DD',
-      dirname: logDir + '/debug', // log file /logs/debug/*.log in save
-      filename: `%DATE%.log`,
-      maxFiles: 30, // 30 Days saved
-      json: false,
-      zippedArchive: true,
-    }),
-    // error log setting
-    new winstonDaily({
-      level: 'error',
-      datePattern: 'YYYY-MM-DD',
-      dirname: logDir + '/error', // log file /logs/error/*.log in save
-      filename: `%DATE%.log`,
-      maxFiles: 30, // 30 Days saved
-      handleExceptions: true,
-      json: false,
-      zippedArchive: true,
-    }),
-  ],
-});
 
-devLogger.add(
-  new winston.transports.Console({
-    format: winston.format.combine(winston.format.splat(), winston.format.colorize()),
-  }),
-);
+const createDevLogger = ()=>{
+  // logs dir
+  const logDir: string = join(__dirname, LOG_DIR);
+
+  if (!existsSync(logDir)) {
+    mkdirSync(logDir);
+  }
+
+  const devLogger = winston.createLogger({
+    format: winston.format.combine(
+      winston.format.timestamp({
+        format: 'YYYY-MM-DD HH:mm:ss',
+      }),
+      logFormat,
+    ),
+    transports: [
+      // debug log setting
+      new winstonDaily({
+        level: 'debug',
+        datePattern: 'YYYY-MM-DD',
+        dirname: logDir + '/debug', // log file /logs/debug/*.log in save
+        filename: `%DATE%.log`,
+        maxFiles: 30, // 30 Days saved
+        json: false,
+        zippedArchive: true,
+      }),
+      // error log setting
+      new winstonDaily({
+        level: 'error',
+        datePattern: 'YYYY-MM-DD',
+        dirname: logDir + '/error', // log file /logs/error/*.log in save
+        filename: `%DATE%.log`,
+        maxFiles: 30, // 30 Days saved
+        handleExceptions: true,
+        json: false,
+        zippedArchive: true,
+      }),
+    ],
+  });
+  
+  devLogger.add(
+    new winston.transports.Console({
+      format: winston.format.combine(winston.format.splat(), winston.format.colorize()),
+    }),
+  );
+
+  return devLogger;
+}
+
 
 const prodLogger = winston.createLogger({
   format: winston.format.combine(
@@ -73,7 +81,7 @@ const prodLogger = winston.createLogger({
   ],
 });
 
-export const logger = process.env.NODE_ENV === 'production' ? prodLogger : devLogger;
+export const logger = process.env.NODE_ENV === 'production' ? prodLogger : createDevLogger();
 
 export const responseLogger = request => {
   const { query } = request.request;

d5e10fccf9f2704505f069c59a46223b338dd1e6
Ikem <ikem.ezechukwu@outlook.com>
2022-11-10 09:29:46 +0100

    The apollo client uri is now solely dependent on the env variable.

--- frontend/apollo-client.ts
+++ frontend/apollo-client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: process.env.CLIENT_URL || 'http://localhost:3002/graphql',
+    uri: process.env.CLIENT_URL,
     cache: new InMemoryCache(),
 });
 
